1041c2496ba91f1b4d5352caabc549136f2641c4
Adibjz <adibjaziri94@gmail.com>
2023-10-12 16:16:42 +0100

    mod

--- shop/file.js
+++ shop/file.js
@@ -80,7 +80,6 @@ var nameFilter = function (name) {
     })
 }
 
-
     function display(item){
     $('#product').append(`
     <div class="oneProduct">
@@ -90,27 +89,27 @@ var nameFilter = function (name) {
     <h3>${item.name}</h3>
     <p><p>Price: ${item.price}</p></p>
     <p><p>Brand:${item.brand}</p></p>
-    <button class="buy"}>Add to Cart</button>
+    <button class="buy">Add to Cart</button>
     </div>
     </div>
     </div>`)
     }  
 
- var sneaker1=makeSneaker("Low Travis Scott Reverse Mocha","269 $","Air jordan",["https://www.stickystore.tn/wp-content/uploads/2023/02/Design-sans-titre-15-300x225.png","https://www.stickystore.tn/wp-content/uploads/2023/02/ipad_travis-scott-x-air-jordan-1-low-og-reverse-mocha-2-600x543.jpeg"])
- var sneaker2=makeSneaker("Air force 1 white","249 $","Nike",["https://www.stickystore.tn/wp-content/uploads/2022/09/Design-sans-titre-30-300x225.png"])
- var sneaker3=makeSneaker("Yeezy boust V2 350","199 $","Adidas",["https://www.stickystore.tn/wp-content/uploads/2023/07/Copie-de-Copie-de-photo-site-4-300x225.png"])
- var sneaker4=makeSneaker("Stan smith","219 $","Adidas",["https://www.stickystore.tn/wp-content/uploads/2022/10/stan-smith-forever-primegreen-white-561991_2000x-removebg-preview-300x225.png"])
- var sneaker5=makeSneaker("550 white green","249 $","New balance",["https://www.stickystore.tn/wp-content/uploads/2022/10/Sans-titre-80-300x225.png"])
- var sneaker6=makeSneaker("A Bathing Ape","299 $","Bape",["https://www.stickystore.tn/wp-content/uploads/2023/10/Design-sans-titre-25-300x225.png"])
- var sneaker7=makeSneaker("Blazer Mid 77 vintage","249 $","Nike",["https://www.stickystore.tn/wp-content/uploads/2022/09/Copie-de-Copie-de-photo-site-1-300x225.png"])
- var sneaker8=makeSneaker("1 High Hyper Royal","259 $","Air jordan",["https://www.stickystore.tn/wp-content/uploads/2022/10/Sans-titre-85-300x225.png"])
- var sneaker9=makeSneaker("530 White Grey Navy","249 $","New balance",["https://www.stickystore.tn/wp-content/uploads/2023/02/New-Balance-530-White-Silver-Navy-1_2000x-300x225.png"])
- var sneaker10=makeSneaker("A Bathing Ape Sta Low Orange","279 $","Bape",["https://www.stickystore.tn/wp-content/uploads/2023/09/Design-sans-titre-11-300x225.png"])
- var sneaker11=makeSneaker("Air Force 1 Low SP Ambush Game Royal","279 $","Nike",["https://www.stickystore.tn/wp-content/uploads/2023/10/Design-sans-titre-30-300x225.png"])
- var sneaker12=makeSneaker("Superstar triple black","229 $","Adidas",["https://www.stickystore.tn/wp-content/uploads/2023/08/Design-sans-titre-18-300x225.png"])
- var sneaker13=makeSneaker("A Bathing Ape  SK8 Sta White Green","299 $","Bape",["https://www.stickystore.tn/wp-content/uploads/2023/10/Design-sans-titre-23-300x225.png"])
- var sneaker14=makeSneaker("Stone lsland x New Balance RC Elite V2 White Red","239 $","New balance",["https://www.stickystore.tn/wp-content/uploads/2023/08/Design-sans-titre-21-300x225.png"])
- var sneaker15=makeSneaker("4 Black Cat","239 $","Air jordan",["https://www.stickystore.tn/wp-content/uploads/2022/11/Design-sans-titre-2-300x225.png"])
+ var sneaker1=makeSneaker("Low Travis Scott Reverse Mocha",269,"Air jordan",["https://www.stickystore.tn/wp-content/uploads/2023/02/Design-sans-titre-15-300x225.png","https://www.stickystore.tn/wp-content/uploads/2023/02/ipad_travis-scott-x-air-jordan-1-low-og-reverse-mocha-2-600x543.jpeg"])
+ var sneaker2=makeSneaker("Air force 1 white",249,"Nike",["https://www.stickystore.tn/wp-content/uploads/2022/09/Design-sans-titre-30-300x225.png"])
+ var sneaker3=makeSneaker("Yeezy boust V2 350",199,"Adidas",["https://www.stickystore.tn/wp-content/uploads/2023/07/Copie-de-Copie-de-photo-site-4-300x225.png"])
+ var sneaker4=makeSneaker("Stan smith",219,"Adidas",["https://www.stickystore.tn/wp-content/uploads/2022/10/stan-smith-forever-primegreen-white-561991_2000x-removebg-preview-300x225.png"])
+ var sneaker5=makeSneaker("550 white green",249,"New balance",["https://www.stickystore.tn/wp-content/uploads/2022/10/Sans-titre-80-300x225.png"])
+ var sneaker6=makeSneaker("A Bathing Ape",299,"Bape",["https://www.stickystore.tn/wp-content/uploads/2023/10/Design-sans-titre-25-300x225.png"])
+ var sneaker7=makeSneaker("Blazer Mid 77 vintage",249,"Nike",["https://www.stickystore.tn/wp-content/uploads/2022/09/Copie-de-Copie-de-photo-site-1-300x225.png"])
+ var sneaker8=makeSneaker("1 High Hyper Royal",259,"Air jordan",["https://www.stickystore.tn/wp-content/uploads/2022/10/Sans-titre-85-300x225.png"])
+ var sneaker9=makeSneaker("530 White Grey Navy",249,"New balance",["https://www.stickystore.tn/wp-content/uploads/2023/02/New-Balance-530-White-Silver-Navy-1_2000x-300x225.png"])
+ var sneaker10=makeSneaker("A Bathing Ape Sta Low Orange",279,"Bape",["https://www.stickystore.tn/wp-content/uploads/2023/09/Design-sans-titre-11-300x225.png"])
+ var sneaker11=makeSneaker("Air Force 1 Low SP Ambush Game Royal",279,"Nike",["https://www.stickystore.tn/wp-content/uploads/2023/10/Design-sans-titre-30-300x225.png"])
+ var sneaker12=makeSneaker("Superstar triple black",229,"Adidas",["https://www.stickystore.tn/wp-content/uploads/2023/08/Design-sans-titre-18-300x225.png"])
+ var sneaker13=makeSneaker("A Bathing Ape  SK8 Sta White Green",299,"Bape",["https://www.stickystore.tn/wp-content/uploads/2023/10/Design-sans-titre-23-300x225.png"])
+ var sneaker14=makeSneaker("Stone lsland x New Balance RC Elite V2 White Red",239,"New balance",["https://www.stickystore.tn/wp-content/uploads/2023/08/Design-sans-titre-21-300x225.png"])
+ var sneaker15=makeSneaker("4 Black Cat",239,"Air jordan",["https://www.stickystore.tn/wp-content/uploads/2022/11/Design-sans-titre-2-300x225.png"])
 
 
  
@@ -271,28 +270,7 @@ var sneakersList=MakeSneakersShop()
         var getnextindex=change1()
         this.src=getnextindex
       })  
-
-      var total=0
-    var cart=[]
-    var addBtn = product.querySelector(".buy");
-          addBtn.addEventListener("click", function(){ addCart(index)});
-
-      function addCart(i) {
-      var a = sneakersList[i];
-          cart.push(a);
-         cartTotal += a.price;
-          var cartItemsList = document.getElementById("items");
-          var cartItem = document.createElement("ul");
-           cartItem.innerHTML =`
-              <span>${a.name}</span>
-              <span>${a.price}DT</span>     
-          `;
-          cartItemsList.appendChild(cartItem);
-       var cartTotalElement = document.getElementById("total");
-          cartTotalElement.textContent = cartTotal.toFixed(2);
-      }
         
-   
 
 
     
