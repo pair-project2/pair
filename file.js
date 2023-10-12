33aea7f29ca36524dbe1baeaae69317a987acae4
Adibjz <adibjaziri94@gmail.com>
2023-10-12 10:03:18 +0100

    login page

--- file.js
+++ file.js
@@ -1,12 +1,14 @@
-function verifyPassword() {
-    var password = document.getElementById("password").value;
-    
-    if (password.length === 0) {
-        alert("Please enter a password.");
-    } else if (password.length < 8) {
-        alert("Password is too short. It must be at least 8 characters long.");
-    } else {
-        alert("Login successful!");
+        function verifyPassword() {
+        var password = document.getElementById("password").value;
 
+        if (password.length === 0) {
+            alert("Please enter a password.");
+        } else if (password.length < 8) {
+            alert("Password is too short. It must be at least 8 characters long.");
+        } else {
+            alert("Login successful!");
+
+            // Redirect to anothor html page
+            window.location.href = "http://127.0.0.1:5500/shop.html"
+        }
     }
-}
